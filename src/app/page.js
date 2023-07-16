79906f14c7723213fee5500d3e09f33ebb3f986c
Rodolfo Valguarnera <Rod7val@gmail.com>
2023-07-16 15:39:22 -0300

    client list example

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,44 @@
-import Image from 'next/image'
+'use client'
+import React, { useState, useEffect } from 'react'
+
+// object to array
+const filterJson = (json) => {
+  var _items = []
+  for (let item in json) {
+    _items = _items.concat(json[item])
+  }
+  return _items
+}  
 
 export default function Home() {
+  const [data, setData] = useState(null)
+ 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://blockchain.info/ticker')
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const result = await response.json()
+      setData(filterJson(result))
+    }
+ 
+    fetchData().catch((e) => {
+      // handle the error as needed
+      console.error('An error occurred while fetching the data: ', e)
+    })
+  }, [])
+
+  const items = (data && data.length) ? data.map((item, key) =>
+    <li key={key.toString()}>
+       <code>${item.last}</code> <b>{`${item.symbol}`}</b>
+    </li>
+  ) : <div>cargando...</div>
+
   return (
-    <main>
-      <div>
-        <p>
-          <code>Hi</code>
-        </p>
-      </div>
-    </main>
+    <div>
+      <b>1 BTC = </b>
+      <ul style={{"padding-left": "20px"}}>{items}</ul>
+    </div>
   )
 }
