62b93b6032bc0b1dca2f02da40ffa1c7d0405882
Rodolfo Valguarnera <Rod7val@gmail.com>
2023-07-19 09:28:38 -0300

    entries obj

--- src/app/page.js
+++ src/app/page.js
@@ -2,23 +2,18 @@
 import React, { useState, useEffect } from 'react'
 
 const apiURL = 'https://blockchain.info/ticker'
-const toArray = (json) => {
-  let _items = []
-  for (let item in json) {
-    _items = _items.concat(json[item])
-  }
-  return _items
-}
 
 export default function Home() {
   const [data, setData] = useState(null)
+
   const fetchData = async (url) => {
     const response = await fetch(url)
     if (!response.ok) {
+      setData([])
       throw new Error(`HTTP error! status: ${response.status}`)
     }
     const result = await response.json()
-    setData(toArray(result))
+    setData(Object.entries(result))
   }
 
   useEffect(() => {
@@ -30,7 +25,7 @@ export default function Home() {
 
   const items = (data && data.length) ? data.map((item, key) =>
     <li key={key.toString()}>
-       <code>${item.last}</code> <b>{`${item.symbol}`}</b>
+       <code>${`${item[1].last}`}</code> <b>{`${item[0]}`}</b>
     </li>
   ) : <div>cargando...</div>
 
